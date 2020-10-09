9eac67fa2553b4cb4b1e60d284fa6e08c3f0efd3
Mizushima Shiba <haidarme@yahoo.co.id>
2020-10-09 11:52:15 +0700

    config heroku

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV === "development") require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const app = express()
-const port = process.env.PORT
 const index = require("./routes/index")
 
 
@@ -19,4 +18,4 @@ app.use(function (error, req, res, next) {
 })
 
 
-app.listen(port, _ => console.log(`Server online on port ${port}`))
+app.listen(process.env.PORT || 3000, _ => console.log(`Server online on port ${process.env.PORT || 3000}`))
