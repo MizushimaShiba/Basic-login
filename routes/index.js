6d028246304cfe058d7d5a98f93d9e9dcad22f65
Mizushima Shiba <haidarme@yahoo.co.id>
2020-10-09 13:39:13 +0700

    redirect to login

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,23 @@ const AuthController = require('../controllers/AuthController')
 const UserController = require('../controllers/UserController')
 const { User } = require('../models')
 
+router.get('/', (req, res) => {
+  if (req.headers.token) {
+    const token = JWT.verify(req.headers.token, process.env.JWT_KEY)
+    if (token) {
+      User.findOne({where: {id: token.id}})
+      .then(data => {
+        if (data) {
+          req.user = token
+          next()
+        } else {
+          next({message: 'Please Login Again!'})
+        }
+      })
+    }
+  } else res.redirect('/login')
+})
+
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
 
